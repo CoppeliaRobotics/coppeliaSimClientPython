630ebaaa1f4616f126ac0404705d01ad73e22ce1
Federico Ferri <federico.ferri.it@gmail.com>
2023-07-13 11:16:46 +0200

    update DYLD_LIBRARY_PATH note

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# if the library has some @executable_path dependencies, it will fail
-# to load; change tose to @rpath via install_name_tool, e.g.:
-# install_name_tool -change @executable_path/../Frameworks/libqscintilla2_qt5.13.dylib @rpath/libqscintilla2_qt5.13.dylib libcoppeliaSim.dylib
-#
-# additionally, need to set DYLD_LIBRARY_PATH=$PWD/../Frameworks
-# to avoid other similar load errors
+# need to set DYLD_LIBRARY_PATH=$PWD/../Frameworks
+# to avoid libraries load errors
 
 from ctypes import *
 import argparse
