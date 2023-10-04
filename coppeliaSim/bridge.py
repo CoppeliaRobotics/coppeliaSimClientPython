0e4f5bd882ef98f9657e7ff0c95d7d3d35f7bd2b
Federico Ferri <federico.ferri.it@gmail.com>
2023-10-04 11:15:20 +0200

    simCallScriptFunctionEx target lua

--- coppeliaSim/bridge.py
+++ coppeliaSim/bridge.py
@@ -3,10 +3,13 @@ from ctypes import *
 from coppeliaSim.lib import *
 
 
+def __f(f):
+    return f + '@lua'
+
 def load():
     stack = simCreateStack()
     simPushStringOntoStack(stack, c_char_p('scriptClientBridge'.encode('ascii')), 0)
-    r = simCallScriptFunctionEx(8, c_char_p('require'.encode('ascii')), stack)
+    r = simCallScriptFunctionEx(8, c_char_p(__f('require').encode('ascii')), stack)
     simReleaseStack(stack)
 
 def call(func, args):
@@ -14,7 +17,7 @@ def call(func, args):
     import cbor
     b = cbor.dumps({'func': func, 'args': args})
     simPushStringOntoStack(stack, c_char_p(b), len(b))
-    r = simCallScriptFunctionEx(8, c_char_p('scriptClientBridge.call'.encode('ascii')), stack)
+    r = simCallScriptFunctionEx(8, c_char_p(__f('scriptClientBridge.call').encode('ascii')), stack)
     sz = c_int()
     ptr = simGetStackStringValue(stack, byref(sz))
     o = cbor.loads(string_at(ptr, sz.value))
