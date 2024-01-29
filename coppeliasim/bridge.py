e4e2c3d36cf219fb736caa5e442f952c35674aff
Federico Ferri <federico.ferri.it@gmail.com>
2024-01-29 16:52:13 +0100

    defer lib imports; switch to direct; drop cbor

--- coppeliasim/bridge.py
+++ coppeliasim/bridge.py
@@ -1,24 +1,29 @@
 from ctypes import *
 
-from coppeliasim.lib import *
-
-
-def __f(f):
-    return f + '@lua'
 
 def load():
-    stack = simCreateStack()
-    simPushStringOntoStack(stack, c_char_p('scriptClientBridge'.encode('ascii')), 0)
-    r = simCallScriptFunctionEx(8, c_char_p(__f('require').encode('ascii')), stack)
-    simReleaseStack(stack)
+    call('require', ('scriptClientBridge',))
 
-def call_direct(func, args):
-    stackHandle = simCreateStack()
+
+def call(func, args):
+    from coppeliasim.lib import (
+        simCreateStack,
+        simCallScriptFunctionEx,
+        simReleaseStack,
+        sim_scripttype_sandboxscript,
+    )
     import coppeliasim.stack as stack
+    stackHandle = simCreateStack()
     stack.write(stackHandle, args)
-    r = simCallScriptFunctionEx(8, c_char_p(func.encode('ascii')), stackHandle)
+    s = sim_scripttype_sandboxscript
+    f = c_char_p(f'{func}@lua'.encode('ascii'))
+    r = simCallScriptFunctionEx(s, f, stackHandle)
     if r == -1:
-        raise Exception(f'simCallScriptFunctionEx(8, {func!r}, {args!r}) returned -1')
+        if False:
+            what = f'simCallScriptFunctionEx({s}, {func!r}, {args!r})'
+        else:
+            what = 'simCallScriptFunctionEx'
+        raise Exception(f'{what} returned -1')
     ret = stack.read(stackHandle)
     simReleaseStack(stackHandle)
     if len(ret) == 1:
@@ -26,27 +31,6 @@ def call_direct(func, args):
     elif len(ret) > 1:
         return ret
 
-def call_cbor(func, args):
-    stack = simCreateStack()
-    import cbor
-    b = cbor.dumps({'func': func, 'args': args})
-    simPushStringOntoStack(stack, c_char_p(b), len(b))
-    r = simCallScriptFunctionEx(8, c_char_p(__f('scriptClientBridge.call').encode('ascii')), stack)
-    sz = c_int()
-    ptr = simGetStackStringValue(stack, byref(sz))
-    o = cbor.loads(string_at(ptr, sz.value))
-    simReleaseBuffer(ptr)
-    simReleaseStack(stack)
-    if o['success']:
-        ret = o['result']
-        if len(ret) == 1:
-            return ret[0]
-        if len(ret) > 1:
-            return tuple(ret)
-    else:
-        raise Exception(o['error'])
-
-call = call_cbor
 
 def getObject(name, _info=None):
     ret = type(name, (), {})
@@ -63,10 +47,12 @@ def getObject(name, _info=None):
             setattr(ret, k, getObject(f'{name}.{k}', _info=v))
     return ret
 
+
 def require(obj):
     call('scriptClientBridge.require', [obj])
     o = getObject(obj)
     return o
+
 
 def getScriptFunctions(scriptHandle):
     return type('', (object,), {
