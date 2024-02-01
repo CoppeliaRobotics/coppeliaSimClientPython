97992a01f1f1f61445e6a0dc6f430ea749ba28dd
Federico Ferri <federico.ferri.it@gmail.com>
2024-02-01 17:22:18 +0100

    get stack type hint from python typing annotations

--- coppeliasim/callback.py
+++ coppeliasim/callback.py
@@ -1,14 +1,32 @@
 def callback(f):
     def wrapper(stackHandle):
         import coppeliasim.stack as stack
+
+        from typing import get_args, Tuple
+
+        inTypes = tuple(
+            [arg_type.__name__ for arg, arg_type in f.__annotations__.items() if arg != 'return']
+        )
+
+        if return_annotation := f.__annotations__.get('return'):
+            origin = getattr(return_annotation, '__origin__', None)
+            if origin in (tuple, list):  # Handling built-in tuple and list
+                outTypes = tuple([t.__name__ for t in get_args(return_annotation)])
+            elif origin:  # Handling other generic types like Tuple, List from typing
+                outTypes = (origin.__name__,)
+            else:
+                outTypes = (return_annotation.__name__,)
+        else:
+            outTypes = ()
+
         try:
-            inArgs = stack.read(stackHandle)
+            inArgs = stack.read(stackHandle, inTypes)
             outArgs = f(*inArgs)
             if outArgs is None:
                 outArgs = ()
             elif not isinstance(outArgs, tuple):
                 outArgs = (outArgs,)
-            stack.write(stackHandle, outArgs)
+            stack.write(stackHandle, outArgs, outTypes)
             return 1
         except Exception:
             import traceback
